50eeeaa7afb6dad4b4cfa3fe86ae3d819fa9c801
Virginia Souza <virginiasouza@Virginias-MacBook-Pro.local>
2017-08-25 17:00:25 -1000

    just working on validation at end

--- calculator.js
+++ calculator.js
@@ -5,61 +5,111 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
+var calculatorModule = (function(){
 
+  var memory = 0;
+  var total = 0;
+
+  var calculator = {
+    load: load,
+    getTotal: getTotal,
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+    recallMemory: recallMemory,
+    saveMemory: saveMemory,
+    clearMemory: clearMemory,
+
+  };
 
   /**
    * sets the `total` to the number passed in
    * @param  { Number } x
    * @return { Number }    current total
    */
-
+   function load(x){
+    total = x;
+     return total;
+    };
 
   /**
    * Return the value of `total`
    * @return { Number }
    */
-
+   function getTotal(){
+    
+    return total;
+  }
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
-
+   function add(x){
+    return total += x;
+   }
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
+   function subtract(x){
 
+    return total -= x;
+   }
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
-
+   function multiply(x){
+    return total *= x;
+   }
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
-
+   function divide(x){
+    return total /= x;
+   }
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
-
+   function recallMemory(){
+    return memory;
+   }
 
   /**
    * Stores the value of `total` to `memory`
    */
-
+   function saveMemory(){
+     memory = total;
+     return memory;
+   }
 
   /**
    * Clear the value stored at `memory`
    */
+   function clearMemory(){
+    memory = 0;
+      return memory;
+   }
 
   /**
    * Validation
    */
+function validate(x){
+  if((x === 'number') === true ){
+
+  }
+}
+
+
+  return calculator;
+
+});
 
